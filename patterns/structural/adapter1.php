5c8d9d86fdabee8a9aabebc3b58f14a8202987b2
subodh-1 <your_email@example.com>
2024-07-10 10:40:36 +0530

    new structural patterns examples

---
+++ patterns/structural/adapter1.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Adapter Pattern
+ * Problem: Allow incompatible interfaces to work together.
+ * When to Use: When you need to integrate a new component into an existing system that doesn't have a compatible interface.
+ * Example: Adapting a new payment gateway to an existing e-commerce system.
+ */
+
+ 
